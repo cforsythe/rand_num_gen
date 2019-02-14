933b9d52c6cf97aba3f4bf495478d711236915b1
cforsythe <cforsythe@csumb.edu>
2019-02-13 23:26:22 -0800

    fixed import

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-import express, { static } from 'express';
-import { join } from 'path';
+const express = require('express');
+const path = require('path');
 
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(static(__dirname + '/dist/rng-web'));
+app.use(express.static(__dirname + '/dist/rng-web'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(join(__dirname+'/dist/rng-web/index.html'));
+res.sendFile(path.join(__dirname+'/dist/rng-web/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
