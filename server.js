95bf439d972b447a7ab6e93f3aee27e75b90111d
cforsythe <cforsythe@csumb.edu>
2019-02-13 23:18:28 -0800

    fixed server port

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 //Install express server
-const express = require('express');
-const path = require('path');
+import express, { static } from 'express';
+import { join } from 'path';
 
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/rng-web'));
+app.use(static(__dirname + '/dist/rng-web'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/rng-web/index.html'));
+res.sendFile(join(__dirname+'/dist/rng-web/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-
+app.listen(process.env.PORT || 8080);
